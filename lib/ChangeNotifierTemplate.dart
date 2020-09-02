545e7378ac65a0664d31c3a40f0bee593b539a71
FaneTheEternal <artemyev.vova@gmail.com>
2020-09-02 15:40:04 +0300

    Поддержка предзаполнения значений

--- lib/ChangeNotifierTemplate.dart
+++ lib/ChangeNotifierTemplate.dart
@@ -9,22 +9,31 @@ mixin ChangeNotifierListMixin<T> on ChangeNotifier {
   final canRemove = false;
   final canAt = false;
 
+  bool beInit = false;
+
   int get count => _list.length;
+
+  initValue() {
+    beInit = true;
+  }
 
   add(T t) {
     if (!canAdd) throw Exception('Cant use add method');
+    if (!beInit) initValue();
     _list.add(t);
     notifyListeners();
   }
 
   remove(int index) {
     if (!canRemove) throw Exception('Cant use remove method');
+    if (!beInit) initValue();
     _list.removeAt(index);
     notifyListeners();
   }
 
   T at(int index) {
     if (!canAt) throw Exception('Cant use at method');
+    if (!beInit) initValue();
     return _list.elementAt(index);
   }
 
