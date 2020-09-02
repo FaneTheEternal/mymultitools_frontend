26bbcb9a49b6f5bf1e6251bc69dc7503cc985cb4
FaneTheEternal <artemyev.vova@gmail.com>
2020-09-02 14:51:49 +0300

    тесты тесты

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,6 +7,8 @@
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mymultitools_frontend/KeyWords.dart';
+import 'package:mymultitools_frontend/apps/notes.dart';
+import 'package:mymultitools_frontend/apps/schedule.dart';
 import 'package:mymultitools_frontend/auth.dart';
 
 import 'package:mymultitools_frontend/main.dart';
@@ -27,6 +29,34 @@ void main() {
       expect(false, equals(authLogoutModel.isLogin));
     });
     authLogoutModel.logout();
+  });
+
+  test('Schedule Notifier', () {
+    final schedule = Schedule();
+    int initCount = 0;
+    schedule.addListener(() {
+      expect(initCount, lessThan(schedule.count));
+    });
+  });
+
+  group('Notes Notifier', () {
+    test('Notes add', () {
+      final notesNotifierAdd = Notes();
+      final initCountAdd = notesNotifierAdd.count;
+      notesNotifierAdd.addListener(() {
+        expect(initCountAdd, lessThan(notesNotifierAdd.count));
+      });
+      notesNotifierAdd.add(Note(name: 'name'));
+    });
+    test('Notes remove', () {
+      final notesNotifierRemove = Notes();
+      notesNotifierRemove.add(Note(name: 'name'));
+      final initCountRemove = notesNotifierRemove.count;
+      notesNotifierRemove.addListener(() {
+        expect(initCountRemove, greaterThan(notesNotifierRemove.count));
+      });
+      notesNotifierRemove.remove(0);
+    });
   });
 
   testWidgets('App widgets', (WidgetTester tester) async {
